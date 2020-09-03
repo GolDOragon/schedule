42ac1a089e24ff09d16593bc3aeee2230a68e197
Viktoryia Kiyanka <mikisum@mail.ru>
2020-09-03 17:35:00 +0300

    feat: add base structure of project

---
+++ scheduale-app/src/components/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+// import your component
+
+export default class App extends Component {
+  render() {
+    return (
+      //add your component into div
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
