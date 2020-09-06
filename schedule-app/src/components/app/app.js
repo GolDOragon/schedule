e643716c74bf359f365fe978c22481870bff7662
fenixoleg7 <fenixoleg7@gmail.com>
2020-09-06 23:39:15 +0300

    feat: Add ant.design in component "List"

--- schedule-app/src/components/app/app.js
+++ schedule-app/src/components/app/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './app.css';
-import Table from '../table/table'
+import Table from '../table/table';
+import List from '../list/list';
 // import your component
 
 const  App = () => {
   return (
     //add your component into div
     <div>
-      <Table />
+        <Table />
+        <hr></hr>
+        <List />
     </div>
   ) 
 }
--- schedule-app/src/components/list/list.js
+++ schedule-app/src/components/list/list.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import './list.css';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Row, Col, Card } from 'antd';
 import ScheduleApiService from '../../services/scheduleApi-service';
+
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
 
 export default class List extends Component {
 
     scheduleApiService = new ScheduleApiService();
 
     state = {
-        event: {}
+        event: {},
+        collapsed: true
     };
 
     constructor() {
         super();
         this.updateEvent();
-    }
+    };
 
     onEventLoaded = (event) => {
         this.setState({event});
@@ -29,7 +40,7 @@ export default class List extends Component {
         this.scheduleApiService
             .getEvent(id)
             .then(this.onEventLoaded);
-    }
+    };
 
     updateOrganizer = () => {
         const id = 'H1DP9yWIwO5CTKBPLllD';
@@ -41,7 +52,7 @@ export default class List extends Component {
                 // return organizer.name;
             })
 
-    }
+    };
     add = () => {
         this.scheduleApiService
             .addEvent({
@@ -50,11 +61,17 @@ export default class List extends Component {
                     timePass: '16h',
                 }
             })
-    }
+    };
     deleteEvent = () =>{
         this.scheduleApiService
             .deleteEvent('yUMFUXCclbHtcmXclBYQ')
-    }
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
 
 
 
@@ -63,9 +80,86 @@ export default class List extends Component {
         const  { event: { name, type, descriptionUrl, description, place},
         }= this.state;
         return (
-            <div>
-
-            </div>
+            <Layout>
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            W1
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            W2
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            W3
+                        </Menu.Item>
+                        <Menu.Item key="4">
+                            W4
+                        </Menu.Item>
+                        <Menu.Item key="5">
+                            W5
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0 }}>
+                        {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                            className: 'trigger',
+                            onClick: this.toggle,
+                        })}
+                    </Header>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        <div className="site-card-border-less-wrapper">
+                            <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
+                                <Col span={6}>
+                                    <Card title="Пн." >
+                                        Card content
+                                    </Card>
+                                </Col>
+                                <Col span={6}>
+                                    <Card title="Вт.">
+                                        Card content
+                                    </Card>
+                                </Col>
+                                <Col span={6}>
+                                    <Card title="Ср.">
+                                        Card content
+                                    </Card>
+                                </Col>
+                                <Col span={6}>
+                                    <Card title="Чт.">
+                                        Card content
+                                    </Card>
+                                </Col>
+                            </Row>
+                            <Row gutter={16}>
+                                <Col span={6}>
+                                    <Card title="Пт." >
+                                        Card content
+                                    </Card>
+                                </Col>
+                                <Col span={6}>
+                                    <Card title="Сб.">
+                                        Card content
+                                    </Card>
+                                </Col>
+                                <Col span={6}>
+                                    <Card title="Вс.">
+                                        Card content
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Content>
+                </Layout>
+            </Layout>
         )
     }
 }
