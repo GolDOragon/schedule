c73e2063dd04ec7dab584c521088a6a172a8035c
Viktoryia Kiyanka <mikisum@mail.ru>
2020-09-20 23:17:52 +0300

    feat: test render input

--- schedule-app/src/components/page/editedPage.js
+++ schedule-app/src/components/page/editedPage.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react'
 import { Card, Input, Form, Radio, Select, DatePicker, TimePicker, InputNumber, Checkbox, Button} from 'antd';
 import moment from 'moment';
 import { EditOutlined, EllipsisOutlined, SettingOutlined} from '@ant-design/icons';
-
+import './page.css';
 const EditedPage = (props)=> {
   const [form] = Form.useForm();
   const {event :{dateTime, time, type, name, timePass, description, descriptionUrl, place, comment, mentor, showComment}, eventId, onUpdateEvent} = props;
   const timeFormat = 'HH:mm';
   const dateFormat = 'YYYY-MM-DD';
   const [componentSize, setComponentSize] = useState('default');
-  const onFormLayoutChange = ({ size }) => {
-    setComponentSize(size);
-  }
+  const [allowComment, setAllowComment] = useState(showComment);
 
+  const onChange = () => {
+    showComment === 'true' ? setAllowComment('false') : setAllowComment('true');
+  }
     return (
       
       <Card
@@ -43,7 +44,8 @@ const EditedPage = (props)=> {
             picture: '',
             video: '',
             map: '',
-            mentor:''
+            mentor:'',
+            showComment: allowComment
 
           }}
           onFinish={(values)=> {
@@ -52,20 +54,10 @@ const EditedPage = (props)=> {
             onUpdateEvent(eventId,values)
           }}
           
-          onValuesChange={onFormLayoutChange}
-          size={componentSize}
         >
-          <Form.Item label="Form Size" name="size">
-            <Radio.Group>
-              <Radio.Button value="small">Small</Radio.Button>
-              <Radio.Button value="default">Default</Radio.Button>
-              <Radio.Button value="large">Large</Radio.Button>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item name='name' label="Название события"><Input /></Form.Item>
+          <Form.Item name='name'><Input /></Form.Item>
           <Form.Item name='type' label="Тип события">
             <Select >
-              <Select.Option >{type}</Select.Option>
               <Select.Option value='Self education'>Self education</Select.Option>
               <Select.Option value='Deadline'>Deadline</Select.Option>
               <Select.Option value='Task'>Task</Select.Option>
@@ -75,15 +67,13 @@ const EditedPage = (props)=> {
               <Select.Option value='Meetup'>Meetup</Select.Option>
             </Select>
           </Form.Item>
-          <Form.Item name='dateTime' label="Дата" ><DatePicker  /></Form.Item>
+          <Form.Item name='dateTime' label="Дата" ><DatePicker /></Form.Item>
           <Form.Item name='time' label='Время' hidden={(type === 'Self education' || type === 'Screening') && true}><TimePicker /></Form.Item>
           <Form.Item name='timePass' label="Время выполнения"><InputNumber formatter={value => `${value}h`} step={0.5} /></Form.Item>
           <Form.Item name='mentorName' label='Имя' ><Input /></Form.Item>
-          <Form.Item name='gitLink' label='Ссылка на профиль'><Input /></Form.Item>
-          <Form.Item name='face' label='Ссылка на фото'><Input /></Form.Item>
           <Form.Item name='description' label='Описание'><Input.TextArea /></Form.Item>
           <Form.Item name='descriptionUrl' label='Ссылка на ТЗ' ><Input /></Form.Item>
-          <Checkbox name='showComment' label="Разрешить оставлять комментарий" >Checkbox</Checkbox>
+          <Checkbox name='showComment' label="Разрешить оставлять комментарий" onChange={onChange}></Checkbox>
           <Form.Item><Button type="primary" htmlType="submit">Сохранить</Button></Form.Item>
         </Form>
       </Card>   
