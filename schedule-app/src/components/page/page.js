7a15b7af7ac6cd849181c145344386973fdaf2f4
Vicktoryia Kiyanko <57707166+Mikisum@users.noreply.github.com>
2020-09-27 23:32:07 +0300

    Page (#30)
    * refactor: changing components for edit event
    
    * feat: add preloader
    
    * fix: fix map
    
    * fix: fix map view
    
    * feat: add router switch
    
    * fix: fix map
    
    * fix: fix styles
    
    * fix: fix styles
    
    * fix: fix language
    
    Co-authored-by: unknown <mikisum@mail.ru[>

--- schedule-app/src/components/page/page.js
+++ schedule-app/src/components/page/page.js
@@ -109,19 +109,19 @@ const Page = (props) => {
           <Organizer organizer={organizer}/>
           }
 
-          {description && <Divider orientation="left">Описание:</Divider>}
+          {description && <Divider orientation="left">Description:</Divider>}
           {description &&
             <p>{description}</p>
           }
-          {descriptionUrl && <Divider orientation="left">Ссылка на тз:</Divider>}
+          {descriptionUrl && <Divider orientation="left">Description Url:</Divider>}
           {descriptionUrl &&
-          <a href={descriptionUrl}>Ссылка на ТЗ</a>
+          <a href={descriptionUrl}>Description Url</a>
           }
           <br />
           {type==='Lecture' &&
           <a href={descriptionUrl}></a>
           }
-          {place && <Divider  orientation="left">Место проведения:</Divider>}
+          {place && <Divider  orientation="left">Place:</Divider>}
           {place &&
             <p>{place}</p>
             
